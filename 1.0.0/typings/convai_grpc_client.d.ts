e3768fe6fbf0147b30df5189c3a32972b5effbec
AirSurfer09 <saurav@convai.com>
2024-07-18 19:43:53 +0530

    custom actions narrative design

---
+++ 1.0.0/typings/convai_grpc_client.d.ts
@@ -0,0 +1,30 @@
+import { grpc } from '@improbable-eng/grpc-web';
+import { GetResponseResponse } from '../Proto/service/service_pb';
+import { ActionConfigParamsType } from './types';
+export declare class ConvaiGRPCClient {
+    client: grpc.Client<any, any>;
+    private feedbackClient;
+    private apiKey;
+    private languageCode;
+    private sessionId;
+    private characterId;
+    private speaker;
+    private inputMode;
+    private isStarted;
+    private speakerId;
+    private disableAudioGeneration;
+    private enableFacialData;
+    private faceModel;
+    private narrativeTemplateKeysMap;
+    private actionConfigParams;
+    constructor(apiKey: string, characterId: string, speaker: string, speakerId: string, sessionId: string, responseCallback: (response: GetResponseResponse) => void, errorCallback: (type: string, statusMessage: string, status: string) => void, languageCode: string, disableAudioGeneration: boolean, enableFacialData: boolean, faceModel: 0 | 1 | 2 | 3, narrativeTemplateKeysMap: Map<string, string>, actionConfig?: ActionConfigParamsType);
+    close(): void;
+    invokeTrigger(name: string, message?: string): void;
+    sendFeedback(interaction_id: string, character_id: string, session_id: string, thumbs_up: boolean, feedback_text: string): void;
+    sendText(text: string): void;
+    sendAudioChunk(chunk: ArrayBuffer): void;
+    finishSend(): void;
+    private setActionConfig;
+    private start;
+}
+export type ConvaiGRPCClientType = ConvaiGRPCClient;
